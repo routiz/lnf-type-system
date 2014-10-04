8036a272891154e816099d4425b7bfc64074ac16
isryu <ryu.insang@gmail.com>
2014-10-04 12:05:04 +0900

    Add project-script

---
+++ project-scripts/indent-sources.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+astyle --style=gnu --indent=spaces=2 *.[ch] src/*.[ch]
---
+++ project-scripts/prepare-commit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./project-scripts/update-tags.sh
+./project-scripts/indent-sources.sh
+./project-scripts/clear-backups.sh
